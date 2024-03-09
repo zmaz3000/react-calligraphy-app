2fc142f99d4aee88b33e8504ca0bb033e65ef510
zmaz3000 <zmaz3000@gmail.com>
2024-03-08 22:37:08 -0600

    Updates

---
+++ precache-manifest.434808d633e67ed41ee685d04582d909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3471c3182a9f23e5bb454e8376f41c4",
+    "url": "/react-calligraphy-app/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/react-calligraphy-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/react-calligraphy-app/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/react-calligraphy-app/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/react-calligraphy-app/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "a8b587c5a2fa71059b63",
+    "url": "/react-calligraphy-app/static/js/runtime~main.663636f2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-calligraphy-app/static/media/logo.06e73328.svg"
+  }
+]);
